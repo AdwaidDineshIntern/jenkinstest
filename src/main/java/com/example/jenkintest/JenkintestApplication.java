0ce4a72d4bcb1516a5ca22d6ff256ce645d85ecf
AdwaidDineshIntern <163625542+AdwaidDineshIntern@users.noreply.github.com>
2024-09-02 23:07:13 +0530

    Update JenkintestApplication.java

--- src/main/java/com/example/jenkintest/JenkintestApplication.java
+++ src/main/java/com/example/jenkintest/JenkintestApplication.java
@@ -2,7 +2,7 @@ package com.example.jenkintest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@SpringBootApplication
 @SpringBootApplication
 public class JenkintestApplication {
 
